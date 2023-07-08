e182e50bb35977aa1025c8747f96e8c76c09818c
lifeng <lifeng@@xbrother.com>
2023-07-08 17:16:52 +0800

    feat: fix do driver

--- drivers/gpio/gpio-xbrother.c
+++ drivers/gpio/gpio-xbrother.c
@@ -1,6 +1,7 @@
 
 
 
+#include "asm-generic/errno-base.h"
 #include "linux/printk.h"
 #include "linux/stddef.h"
 #include <linux/kobject.h>
@@ -13,9 +14,91 @@
 #include <linux/pinctrl/pinctrl.h>
 #include <linux/slab.h>
 #include <linux/platform_device.h>
+#include <linux/clk.h>
+
+struct iio_xbro{
+    int gpio;
+    unsigned long flags;
+    const char * name;
+    unsigned int chip;
+    unsigned int channel;
+};
 
 
+static struct iio_xbro iio_xbro_list[]={
+    {
+        .gpio=100,
+        .flags=GPIOF_OUT_INIT_LOW,
+        .name="do01",
+    },
+    {
+        .gpio=101,
+        .flags=GPIOF_OUT_INIT_LOW,
+        .name="do02",
+    },
+    {
+        .gpio=104,
+        .flags=GPIOF_OUT_INIT_LOW,
+        .name="do03",
+    },
+    {
+        .gpio=105,
+        .flags=GPIOF_OUT_INIT_LOW,
+        .name="do04",
+    },
+    {
+        .gpio=-1,
+        .name="ai01",
+        .chip=0,
+        .channel=0,
+    },
+    {
+        .gpio=-1,
+        .name="ai02",
+        .chip=0,
+        .channel=1,
+    },
+    {
+        .gpio=-1,
+        .name="ai03",
+        .chip=0,
+        .channel=2,
+    },
+    {
+        .gpio=-1,
+        .name="ai04",
+        .chip=0,
+        .channel=3,
+    },
+    {
+        .gpio=-1,
+        .name="ai05",
+        .chip=0,
+        .channel=4,
+    },
+    {
+        .gpio=-1,
+        .name="ai06",
+        .chip=0,
+        .channel=5,
+    },
+    {
+        .gpio=-1,
+        .name="ai07",
+        .chip=0,
+        .channel=6,
+    },
+    {
+        .gpio=-1,
+        .name="ai08",
+        .chip=0,
+        .channel=7,
+    },
+};
 static struct kobject *kobj_xbro;
+
+
+
 
 
 #define MAX_XBR_ATTR_NUM (128)
@@ -26,35 +109,44 @@ static struct kobject *kobj_xbro;
 
 static ssize_t attr_show(struct kobject *kobj, struct kobj_attribute *attr_kobj,
 			char *buf);
-
+static ssize_t attr_store(struct kobject *kobj, struct kobj_attribute *attr,
+			 const char *buf, size_t count);
 static  struct kobj_attribute attr_all []={
     {
         .attr={
             .name="do01",
-            .mode=VERIFY_OCTAL_PERMISSIONS(0444),
+            .mode=VERIFY_OCTAL_PERMISSIONS(0664),
         },
         .show=attr_show,
-        .store=NULL,
+        .store=attr_store,
     },
     {
         .attr={
             .name="do02",
-            .mode=VERIFY_OCTAL_PERMISSIONS(0444),
+            .mode=VERIFY_OCTAL_PERMISSIONS(0664),
         },
         .show=attr_show,
-        .store=NULL,
+        .store=attr_store,
     },
     {
         .attr={
             .name="do03",
-            .mode=VERIFY_OCTAL_PERMISSIONS(0444),
+            .mode=VERIFY_OCTAL_PERMISSIONS(0664),
         },
         .show=attr_show,
-        .store=NULL,
+        .store=attr_store,
     },
     {
         .attr={
             .name="do04",
+            .mode=VERIFY_OCTAL_PERMISSIONS(0664),
+        },
+        .show=attr_show,
+        .store=attr_store,
+    },
+    {
+        .attr={
+            .name="ai01",
             .mode=VERIFY_OCTAL_PERMISSIONS(0444),
         },
         .show=attr_show,
@@ -62,7 +154,7 @@ static  struct kobj_attribute attr_all []={
     },
     {
         .attr={
-            .name="do05",
+            .name="ai02",
             .mode=VERIFY_OCTAL_PERMISSIONS(0444),
         },
         .show=attr_show,
@@ -70,7 +162,7 @@ static  struct kobj_attribute attr_all []={
     },
     {
         .attr={
-            .name="do06",
+            .name="ai03",
             .mode=VERIFY_OCTAL_PERMISSIONS(0444),
         },
         .show=attr_show,
@@ -78,7 +170,39 @@ static  struct kobj_attribute attr_all []={
     },
     {
         .attr={
-            .name="do07",
+            .name="ai04",
+            .mode=VERIFY_OCTAL_PERMISSIONS(0444),
+        },
+        .show=attr_show,
+        .store=NULL,
+    },
+    {
+        .attr={
+            .name="ai05",
+            .mode=VERIFY_OCTAL_PERMISSIONS(0444),
+        },
+        .show=attr_show,
+        .store=NULL,
+    },
+    {
+        .attr={
+            .name="ai06",
+            .mode=VERIFY_OCTAL_PERMISSIONS(0444),
+        },
+        .show=attr_show,
+        .store=NULL,
+    },
+    {
+        .attr={
+            .name="ai07",
+            .mode=VERIFY_OCTAL_PERMISSIONS(0444),
+        },
+        .show=attr_show,
+        .store=NULL,
+    },
+    {
+        .attr={
+            .name="ai08",
             .mode=VERIFY_OCTAL_PERMISSIONS(0444),
         },
         .show=attr_show,
@@ -91,12 +215,55 @@ static struct attribute_group attr_group_xbro = {
 	.attrs = attrs_xbro,
 };
 
+static ssize_t attr_store(struct kobject *kobj, struct kobj_attribute *attr_kobj,
+			 const char *buf, size_t count)
+{
 
+    
+	int index,number, ret;
+
+    index=   attr_kobj-attr_all;
+    if(index <0 || index >=sizeof(attr_all)/(sizeof(struct kobj_attribute))){
+        return -EINVAL;
+    }
+	ret = kstrtoint(buf, 10, &number);
+	if (ret < 0)
+		return ret;
+    if(iio_xbro_list[index].gpio>=0){
+        gpio_set_value_cansleep(iio_xbro_list[index].gpio,!!number);
+    }
+    return count;
+}
 
 static ssize_t attr_show(struct kobject *kobj, struct kobj_attribute *attr_kobj,
 			char *buf){
-    int i=   attr_kobj-attr_all;
-    pr_info("%d",i);
+    int index;
+    index=   attr_kobj-attr_all;
+    if(index <0 || index >=sizeof(attr_all)/(sizeof(struct kobj_attribute))){
+        return -EINVAL;
+    }
+    if(iio_xbro_list[index].gpio>=0){
+        return sprintf(buf, "%d\n", gpio_get_value_cansleep(iio_xbro_list[index].gpio));
+    }else{  //do AD sample
+        return -EINVAL;
+    }
+    return -EAGAIN;
+}
+
+
+static int __init nuc980nadc_init(void){
+    struct clk * clk_eclk;
+    struct clk * clk_adc;
+ 
+    clk_eclk=clk_get(NULL, "adc_eclk");
+    
+    clk_prepare(clk_eclk);
+    clk_enable(clk_eclk);
+    clk_set_rate(clk_eclk, 4000000);
+
+    clk_adc= clk_get(NULL, "adc");
+    clk_prepare(clk_adc);
+    clk_enable(clk_adc);
     return 0;
 }
 
@@ -104,6 +271,7 @@ static int __init nuc980iio_init(void)
 {
     int err=0;
     int attr_num=sizeof(attr_all)/(sizeof(struct kobj_attribute));
+    int iio_num=sizeof(iio_xbro_list)/sizeof(struct iio_xbro);
     int i=0;
     for(i=0;i<attr_num;i++){
         attrs_xbro[i]=&attr_all[i].attr;
@@ -120,8 +288,16 @@ static int __init nuc980iio_init(void)
 	if (err)
 		kobject_put(kobj_xbro);
 
-
-	pr_info("fmt, ...");
+    for(i=0;i<iio_num;i++){
+        if(iio_xbro_list[i].gpio<0){
+            continue;
+        }
+       if(gpio_request_one(iio_xbro_list[i].gpio,iio_xbro_list[i].flags,iio_xbro_list[i].name)){
+            pr_err("gpio%d request failed",iio_xbro_list[i].gpio);
+       }
+    }
+    nuc980nadc_init();
+	pr_info("xbrother iio init\n");
     return 0;
 }
 
@@ -130,9 +306,8 @@ static void __exit nuc980iio_exit(void)
 	
 }
 
-module_init(nuc980iio_init);
-module_exit(nuc980iio_exit);
 
+late_initcall(nuc980iio_init);
 
 MODULE_LICENSE("GPL");
 MODULE_DESCRIPTION("NUC980 xbrother driver");
